00a0c271575329cea017bc828fd73d3dc063e1af
Riccardo De Maria <riccardodemaria@gmail.com>
2016-04-04 11:11:57 +0200

    update

--- cernlib-f90-1/erfr.f90
+++ cernlib-f90-1/erfr.f90
@@ -14,15 +14,15 @@
     real(8), INTENT(OUT):: wx,wy
     real(8) x,y,q,h,xl,xh,yh,tx,ty,tn,sx,sy,saux,  &
          rx(33),ry(33),cc,xlim,ylim,fac1,fac2,fac3
-    parameter(cc=1.12837916709551,        &
-         xlim=5.33,ylim=4.29,fac1=3.2,fac2=23.0,fac3=21.0)
+    parameter(cc=1.12837916709551D0,        &
+         xlim=5.33D0,,ylim=4.29D0,,fac1=3.2D0,,fac2=23,,fac3=21)
 
     x = abs(xx)
     y = abs(yy)
 
     if (y .lt. ylim  .and.  x .lt. xlim) then
-       q  = (1.0 - y / ylim) * sqrt(1.0 - (x/xlim)**2)
-       h  = 1.0 / (fac1 * q)
+       q  = (1.0D0 - y / ylim) * sqrt(1.0D0 - (x/xlim)**2)
+       h  = 1.0D0 / (fac1 * q)
        nc = 7 + int(fac2*q)
        xl = h**(1 - nc)
        xh = y + 0.5/h
@@ -35,8 +35,8 @@
           tx = xh + n * rx(n+1)
           ty = yh - n * ry(n+1)
           tn = tx*tx + ty*ty
-          rx(n) = 0.5 * tx / tn
-          ry(n) = 0.5 * ty / tn
+          rx(n) = 0.5D0 * tx / tn
+          ry(n) = 0.5D0 * ty / tn
        enddo
 
        sx = 0.0
@@ -61,8 +61,8 @@
           tx = xh + n * rx(1)
           ty = yh - n * ry(1)
           tn = tx*tx + ty*ty
-          rx(1) = 0.5 * tx / tn
-          ry(1) = 0.5 * ty / tn
+          rx(1) = 0.5D0 * tx / tn
+          ry(1) = 0.5D0 * ty / tn
        enddo
 
        wx = cc * rx(1)
@@ -71,8 +71,8 @@
 
     !      if(y .eq. zero) wx = exp(-x**2)
     if(yy .lt. 0.0) then
-       wx =   2.0 * exp(y*y-x*x) * cos(2.0*x*y) - wx
-       wy = - 2.0 * exp(y*y-x*x) * sin(2.0*x*y) - wy
+       wx =   2.0D0 * exp(y*y-x*x) * cos(2.0D0*x*y) - wx
+       wy = - 2.0D0 * exp(y*y-x*x) * sin(2.0D0*x*y) - wy
        if(xx .gt. 0.0) wy = -wy
     else
        if(xx .lt. 0.0) wy = -wy
